ce7562b8b16d1b534fd731b2f1aad19d315e4fd1
sualan499 <sualan64@gmail.com>
2022-03-24 10:12:48 -0400

    j

---
+++ src/textadventure/Items.java
@@ -0,0 +1,28 @@
+package textadventure;
+
+import java.io.IOException;
+
+
+public class Items {
+	String name; 
+	 String description; 
+//	 boolean isCarryable = true;
+//	 boolean isActivated = false; 
+//	 String descrActive = ""; 
+	
+	 String readText = ""; 
+	 void ItmInfo(String Itemname, String itemdesc){
+		 name=Itemname;
+		 desc=Itemdesc;
+	 }
+
+	
+
+	
+	public static void main(String[] args) {
+		
+
+			
+		
+	}
+}
